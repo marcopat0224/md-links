fb00b09de4d058569c834e0e6c894fed2800b62c
yuriquezada <yuri_quezada@hotmail.com>
2019-02-25 18:56:26 -0500

    Implementa test de typeof convertirAbsoluta

---
+++ test/index.spec.js
@@ -0,0 +1,8 @@
+import { convertirAbsoluta } from '../src/main'
+
+
+describe('convertirAbsoluta', ()=>{
+  it ('Debería ser una función', ()=>{
+    expect (typeof convertirAbsoluta).toBe('function');
+  })
+})
