4517e2d7a3defbc63aa1a1b99fb4c6117ff2fb02
yuriquezada <yuri_quezada@hotmail.com>
2019-03-08 05:50:38 -0500

    Muestra en la función extractLinks un arreglo de links con las propiedades href, text y file

--- src/extract-links.js
+++ src/extract-links.js
@@ -1,27 +1,22 @@
-const path = require('path');
 const fs = require('fs');
+const marked = require('marked');
 
-// export const readFile = ruta => {
-//   const leerArchivo = fs.readFileSync(ruta).toString();
-//   return leerArchivo;
-// }
-
-// export const readFiles = array => {
-//   array.forEach(elem => {
-//     const mdFileContent = readFile(elem);
-//     return mdFileContent;
-//   })
-// }
-
-export const extractLinks = (mdFilePathsArray) => {
+const extractLinks = (array) => {
 	let linkArray = [];
-	mdFilePathsArray.forEach((route) => {
-		const readFiles = fs.readFileSync(route, 'utf-8');
+	array.forEach((file) => {
+		const readFiles = fs.readFileSync(file, 'utf-8');
 		const renderer = new marked.Renderer();
 		renderer.link = (href, __, text) => {
-			linkArray.push({ href, text: text.substring(0, 50), route });
+			linkArray.push({ href, text: text.substring(0, 50), file });
 		};
 		marked(readFiles, { renderer });
 	});
 	return linkArray;
 };
+
+const mdFilePathsArray =
+	['C:\\Users\\USER T430\\Documents\\Project\\LIM008-fe-md-links\\test\\probando-mdlinks\\lalala.md',
+		'C:\\Users\\USER T430\\Documents\\Project\\LIM008-fe-md-links\\test\\probando-mdlinks\\more\\Readmetest.md',
+		'C:\\Users\\USER T430\\Documents\\Project\\LIM008-fe-md-links\\test\\probando-mdlinks\\README.md'];
+
+console.log(extractLinks(mdFilePathsArray));
--- test/extract-links.spec.js
+++ test/extract-links.spec.js
@@ -1,3 +1,13 @@
 import { extractLinks } from "../src/extract-links";
+import { mdFilePathsArray } from "./extract-md.spec";
 
-describe('extractLinks')
+
+
+describe('extractLinks', () => {
+  it('Debería ser una función', () => {
+    expect(typeof extractLinks).toBe('function')
+  })
+  it('Debería retornar un arreglo de links con las propiedades href, text, file; al ingresar un arreglo de archivos markdown', ()=>{
+    expect(extractLinks(mdFilePathsArray)).toEqual()
+  })
+})
