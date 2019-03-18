ef6194026bdae4ac8cd976f4f47855ce497eb85a
yuriquezada <yuri_quezada@hotmail.com>
2019-03-18 09:26:50 -0500

    Testeo de md-links al 100%

--- src/md-links/md-links.js
+++ src/md-links/md-links.js
@@ -3,37 +3,21 @@ import { filterMdFiles } from '../extract-md.js';
 import { extractLinks } from '../extract-links.js';
 import { validateLink } from '../validate.js';
 
-const relativePath = '..\\test\\probando-mdlinks';
-
-export const mdLinks = (route, option) => {
-  return new Promise((resolve, reject) => {
-    let rutaAbsoluta;
-    if (isPathAbsolute(route) === false) {
-      rutaAbsoluta = convertPathRelativeToAbsolute(route);
-    } else {
-      rutaAbsoluta = route;
-    };
-
-    const arrFiles = readDirectory(route);
-    const arrMd = filterMdFiles(arrFiles);
-    const arrLinks = extractLinks(arrMd);
-    if (option.validate === true) {
-      validateLink(arrLinks)
-        .then(response => {
-          resolve(response);
-        })
-        .catch(err => {
-          resolve(err);
-        });
-    } else {
-      resolve(arrLinks);
-    }
+export const mdLinks = (path, options) => {
+  let pathAbs;
+  if (!isPathAbsolute(path)) {
+    pathAbs = convertPathRelativeToAbsolute(path);
+  } else {
+    pathAbs = path;
+  };
+  return new Promise((resolve) => {
+    if (!options.validate) {
+      resolve(extractLinks(filterMdFiles(readDirectory(pathAbs))));
+    } if (options.validate) {
+      resolve(validateLink(extractLinks(filterMdFiles(readDirectory(pathAbs)))));
+    } 
   });
 };
 
-// mdLinks(relativePath, true)
-//   .then(result => console.log(result))
-//   .catch(err => err);
-
-// mdLinks(relativePath, { validate: true })
+// mdLinks('test\\pruebastest', {validate: true})
 //   .then(res => console.log(res));
